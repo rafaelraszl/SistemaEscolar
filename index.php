68c4efd6af696ca0dd2f181ee2ee6c105a482337
Rafael Raszl <rafaelmsraszl@gmail.com>
2018-07-19 02:28:19 -0300

    Criada  a classe PageAdmin e Tpl Admin

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Mekhet\Page;
+use \Mekhet\PageAdmin;
 
 $app = new Slim();
 
@@ -15,6 +16,14 @@ $app->config('debug', true);
 $app->get('/', function() {
 
 	$page = new Page();
+
+	$page->setTpl("index");
+
+});
+
+$app->get('/admin', function() {
+
+	$page = new PageAdmin();
 
 	$page->setTpl("index");
 
